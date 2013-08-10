21e96a96f7f8e6424cd700e19c85caefc9d45faf
Samuel Taylor <github@samueltaylor.org>
2013-08-10 11:33:29 -0700

    Basic/skeleton setup

---
+++ api/message.php
@@ -0,0 +1,8 @@
+<?php
+define("CODE_DEFAULT", 0);
+
+$response = array("response" => CODE_DEFAULT, "message" => "Default");
+
+echo json_encode($response);
+?>
+
---
+++ script/tmlm.js
@@ -0,0 +1,28 @@
+function onLoad() {
+    window.console && console.log("Hello");
+}
+
+function onGoClicked() {
+    var request;
+    try {
+        request = new XMLHttpRequest();
+    } catch (e) {
+        request = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    request.open("GET", "", true);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            var response = eval("(" + request.responseText + ")");
+            document.getElementById("msg").innerHTML = response["message"]; 
+        }
+    };
+    var msg = document.getElementById("msg_in").value;
+    request.open("GET", "./script/message.php?msg=" + msg, true);
+    request.send();
+}
+
+if (window.addEventListener) {
+    window.addEventListener("load", onLoad, false);
+} else if (window.attachEvent) {// IE
+    window.attachEvent("onload", onLoad);
+}
