e9de92991023cc092d4045c2786eb261614c2288
Samuel Taylor <github@samueltaylor.org>
2013-08-10 14:55:10 -0700

    message API works as expected

--- api/message.php
+++ api/message.php
@@ -1,12 +1,48 @@
 <?php
-define("CODE_DEFAULT", 0);
+define("CODE_NO_CREDS", 501);
+define("CODE_EMPTY_MSG", 400);
+define("CODE_SQL_ERR", 500);
+define("CODE_SUCCESS", 200);
+define("DEBUG", true);
 
 header("Content-Type: application/json; charset=utf-8");
+include "/var/www_be/tmlm/creds.php";
 
-$response = array("response" => CODE_DEFAULT, "message" => "Default message");
+$response = array("response" => CODE_NO_CREDS, "message" => "Server error");
 
-if (!empty($_GET["msg"])) {
-    $response["message"] = $_GET["msg"];
+if (!isset($un) || !isset($pw)) {
+    die(json_encode($response));
+}
+if (empty($_GET["msg"])) {
+    $response["response"] = CODE_EMPTY_MSG;
+    $response["message"]  = "Empty message";
+    die(json_encode($response));
+}
+
+$db = new PDO("mysql:host=localhost;dbname=tmlm;charset=utf8", $un, $pw, 
+    array(PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+try {
+    // Insert the given message
+    $stmt = $db->prepare("INSERT INTO messages(used, message) VALUES(0, '" .
+        $_GET["msg"] . "')");
+    $stmt->execute();
+    // Get an unused message
+    $stmt = $db->query("SELECT message FROM `messages` WHERE used = 0 LIMIT 1");
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    $msg = $row["message"];
+    // Set that message to be used
+    $stmt = $db->prepare("UPDATE messages SET used = 1 " .
+        "WHERE message = '$msg' AND used = 0 LIMIT 1");
+    $stmt->execute();
+
+    $response["response"] = CODE_SUCCESS;
+    $response["message"]  = $msg;
+} catch (PDOException $e) {
+    $response["response"] = CODE_SQL_ERR;
+    $response["message"]  = "SQL error";
+    if (DEBUG) { $response["message"] .= " " . $e->getMessage(); }
+    die(json_encode($response));
 }
 
 echo json_encode($response);
